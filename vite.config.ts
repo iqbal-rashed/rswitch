e55eca2c1314d01b8f7225fbb479b28a81582b92
iqbal-rashed <dev.rashediqbal@gmail.com>
2023-08-01 17:25:29 +0600

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,11 @@
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
-
 import dts from "vite-plugin-dts";
-import EsLint from "vite-plugin-linter";
 import tsConfigPaths from "vite-tsconfig-paths";
-const { EsLinter, linterPlugin } = EsLint;
 
-export default defineConfig((configEnv) => ({
+export default defineConfig(() => ({
     plugins: [
         tsConfigPaths(),
-        linterPlugin({
-            include: ["./src}/**/*.{ts}"],
-            linters: [new EsLinter({ configEnv })],
-        }),
         dts({
             include: ["src/"],
         }),
