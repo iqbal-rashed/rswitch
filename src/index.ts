1c98c1356eada668f0a3ae845ec940e212f63b26
Rashed Iqbal <dev.rashediqbal@gmail.com>
2023-09-12 23:13:36 +0600

    add some changes

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,34 @@
 type RSwitch = {
-    [key: string]: any;
+  [key: string]: any;
 };
 
 type Options = {
-    returnFunction?: boolean;
+  returnFunction?: boolean;
 };
 
-export function rswitch(
-    key: string | undefined,
-    rswitch: RSwitch,
-    options?: Options
+function rswitch(
+  key: string | undefined,
+  rswitch: RSwitch,
+  options?: Options
 ): any {
-    const fnCall = options?.returnFunction || false;
-    const value = rswitch[key!];
-    const keys = Object.keys(rswitch)
-        .filter((v) => v.includes(","))
-        .flatMap((v) => v.split(","))
-        .map((v) => v.trim());
-    if (!value) {
-        if (keys.includes(key!)) {
-            return typeof value === "function" && !fnCall ? value() : value;
-        }
-        const _default = rswitch[""];
-        return typeof _default === "function" && !fnCall
-            ? _default()
-            : _default;
-    } else {
-        return typeof value === "function" && !fnCall ? value() : value;
+  const fnCall = options?.returnFunction || false;
+
+  const value = rswitch[key!];
+
+  const filtered = Object.keys(rswitch).filter((v) => v.includes(","));
+
+  const keys = filtered.flatMap((v) => v.split(",")).map((v) => v.trim());
+  if (!value) {
+    if (keys.includes(key!)) {
+      const findKey = filtered.find((v) => v.includes(key!));
+      const multiValue = rswitch[findKey!];
+      return typeof multiValue === "function" && !fnCall
+        ? multiValue()
+        : multiValue;
     }
+    const _default = rswitch[""];
+    return typeof _default === "function" && !fnCall ? _default() : _default;
+  } else {
+    return typeof value === "function" && !fnCall ? value() : value;
+  }
 }
